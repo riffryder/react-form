416d97dd537511493fab396c5272e06f2e2e4831
Raphael Croce <rcroce7@gmail.com>
2019-01-23 14:45:23 -0600

    Implement layout grid for welcome header and form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import PageForm from './components/PageForm.js';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       React Form
-      </div>
+      <Grid className="wrapper">
+        <Row>
+          <Col xs={12} md={7}>
+            <header>
+              <h1>Welcome</h1>
+              <p>Please tell us a bit about yourself to get started.</p>
+            </header>
+          </Col>
+          <Col xs={12} md={5}>
+            <PageForm />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
