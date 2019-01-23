3a7b51eaa62321364aea7f89b57888ab5b2436a5
Raphael Croce <rcroce7@gmail.com>
2019-01-23 15:03:50 -0600

    Add image assets and include logo app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 import PageForm from './components/PageForm.js';
+import logo from './assets/GiantRobotLTD_Logo.svg';
 
 import './App.css';
 
@@ -10,7 +11,8 @@ class App extends Component {
     return (
       <Grid className="wrapper">
         <Row>
-          <Col xs={12} md={7}>
+          <Col xs={12} md={7} className="header">
+            <img src={logo} alt="logo" className="logo"/>
             <header>
               <h1>Welcome</h1>
               <p>Please tell us a bit about yourself to get started.</p>
